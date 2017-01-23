1d1b0f7d643c4cae06dac6b043cde8ed59908722
Jared Pochtar <jpochtar@gmail.com>
2017-01-23 09:01:51 -0500

    electron/uploadcare demo

--- main.js
+++ main.js
@@ -16,11 +16,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.loadURL("https://uploadcare.com/documentation/widget/")
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
